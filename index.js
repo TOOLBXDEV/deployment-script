362bf7743f13f99f4370ac3cc4806300eef87614
Yu Li <contact+github@yuli.se>
2023-08-28 11:22:05 -0400

    Improve the way PRs are fetched
    This commit implements a new way to fetch PRs. Before the change, the
    code used old Github APIs to fetch and filter through PRs by pages,
    trying to find the PRs that match the new commit refs. The new code uses
    new Github API to get the PRs that match the new commit refs directly.

--- index.js
+++ index.js
@@ -24,8 +24,9 @@ function getConfig() {
   };
 }
 
+const fetch = require('node-fetch');
 const config = getConfig();
-const octokit = new (require('octokit').Octokit)({ auth: config.token });
+const octokit = new (require('octokit').Octokit)({ auth: config.token, request: {fetch} });
 const baseOctokitArgs = {
   owner: config.owner,
   repo: config.repo
@@ -41,87 +42,60 @@ async function main() {
   await promptForDeployment();
 }
 
+async function getPullRequestByCommitSha(sha, config) {
+  const response = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
+    owner: config.owner,
+    repo: config.repo,
+    commit_sha: sha,
+  });
+  // Unless I'm wrong, this should only ever be one PR per commit, to the master
+  // branch. However, the API returns an array, so we'll just filter it to be
+  // safe.
+  const onlyToMaster = response.data.filter((pr) => pr.base.ref === 'master'); 
+  if (onlyToMaster.length === 0) {
+    console.log(`No PRs found for commit ${sha}`);
+    process.exit(1);
+  }
+  if (onlyToMaster.length > 1) {
+    console.log(`Found ${onlyToMaster.length} PRs for commit ${sha}. There should only be one. Cancelling deployment. This suggests that something is wrong with the deployment script.`);
+    process.exit(1);
+  }
+  return onlyToMaster[0];
+}
+
 /**
  * Find the pull requests that exist in staging, but not in production.
- *
- * The idea is, every commit on the default branch corresponds to a pull request, since a direct
- * push is not allowed to it. That is, any changes on the default branch can be made only through
- * pull requests.
- *
- * Keeping this in mind, to find out all pull requests that exist in staging but not in production,
- * we can:
- *
- * - Find out the commits between "staging" and "production" tags. Since the tags point to commits
- *   in the default branch, every such commit corresponds to a pull request. Hence, this gives us
- *   the pull requests that exist in staging, but not in production.
- *
- * - Fetch merged pull requests from GitHub and filter them by the refs that we discovered in the
- *   previous step.
- *
- * On another note, it might be possible to "improve the performance" (very very negligibly) by
- * assuming that the PRs fetched from GitHub will be in the same order of the ref order between the
- * "production" and "staging" tags. Although I believe that this is currently a correct assumption,
- * any "performance gains" would not be worth the risk of making the program less correct.
- *
- * However, if there was no such risk, the "performance improvement" would have been achieved as
- * follows: The first (couple) PRs fetched would possibly be the PRs that have just been merged and
- * hence, those PRs wouldn't even be in staging yet. So, starting from the latest PR, we would
- * search for the first PR that is in staging. After finding it, we would simply splice the PR array
- * as:
- *
- *  - Starting index: Index of the first PR that is in staging.
- *  - Ending index: Starting index + number of commits between staging and production.
- *
- * However, this is not too straightforward either, since an "edge case" is that the number of PRs
- * since the last deployment to production are very high, like almost 100. In that case, we would
- * need to fetch the next group of PRs and do the "splice" operation over two arrays. If the number
- * is even more, then we would need to do it over more arrays, etc. All of this extra cases that
- * need to be accommodated would increase the risk of introducing bugs.
  */
 async function displayNewPRs() {
-  console.log('Fetching the new pull requests since the last deployment to production.');
+  console.log('Fetching the new pull requests since the last deployment to production.\n');
 
   const newRefs = await fetchNewRefs();
-  const newPRs = [];
-  let pullsRequestsPageNumber = 1;
+  const mapRefToPR = new Map();
 
-  find_new_prs:
-  while (true) {
-    const mergedPRs = await fetchMergedPRs(pullsRequestsPageNumber);
-
-    if (mergedPRs.length === 0) {
-      console.log("No merged PRs found. This means that there is either something wrong the repo or with this program. Cancelling deployment.");
-      process.exit(1);
-    }
-
-    for (const pr of mergedPRs) {
-      if (newRefs.includes(pr.merge_commit_sha)) {
-        newPRs.push(pr);
-
-        if (newPRs.length === newRefs.length) break find_new_prs;
-      }
-    }
-    // If we are here, we weren't able to find all PRs. Hence, we need to fetch the next batch of
-    // merged PRs and repeat.
-    pullsRequestsPageNumber++;
+  for (const ref of newRefs) {
+    const pr = await getPullRequestByCommitSha(ref, config);
+    mapRefToPR.set(ref, pr);
   }
 
   console.log('Pull requests deployed to staging since the last deployment to production are:\n');
 
-  const { cyanBright, green }  = require('chalk')
+  const { cyanBright, green, grey }  = require('chalk')
   const { DateTime: { fromISO, DATETIME_MED} } = require('luxon');
 
   const uniqueUsers = new Set();
 
-  for (let i = 0; i < newPRs.length; ++i) {
-    const pr = newPRs[i];
+  for (const [i, ref] of newRefs.entries()) {
+    const pr = mapRefToPR.get(ref);
 
     const userName = cyanBright(pr.user.login + ':');
     const mergeDate = green(fromISO(pr.merged_at).toLocaleString(DATETIME_MED));
+    const shortSha = grey(ref.slice(0, 7));
+    const webUrl = grey(pr.html_url);
 
     uniqueUsers.add(pr.user.login);
 
-    console.log(`(${i + 1}) ${userName} ${pr.title} (${mergeDate})`);
+    console.log(`(${i + 1}) ${userName} ${pr.title} (${shortSha}, ${mergeDate})`);
+    console.log(`     ${webUrl}`);
   }
 
   console.log(`\nUnique User List[${uniqueUsers.size}]: ${Array.from(uniqueUsers).join(' ')}`);
